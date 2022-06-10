bdc6007fb0d1e1b72183186d5b7c086173e4082f
coderblack <coderblack@163.com>
2022-06-10 16:14:06 +0800

    生活不止眼前的苟且，还有诗和远方

---
+++ flink_course/src/main/java/cn/doitedu/flinksql/demos/Demo6_Exercise.java
@@ -0,0 +1,15 @@
+package cn.doitedu.flinksql.demos;
+
+
+/**
+ * kafka中有如下数据：
+ * {"id":1,"name":{"formal":"zs","nick":"tiedan"},"age":18,"gender":"male"}
+ *
+ *
+ * 现在需要用flinksq来对上述数据进行查询统计：
+ *   截止到当前,每个昵称,都有多少个用户
+ *   截止到当前,每个性别,年龄最大值
+ *
+ */
+public class Demo6_Exercise {
+}
